b8dfbfb04bc79a18760105a61bfcff3a3a161b62
kam <git@techniworm.dev>
2020-09-03 20:31:36 +0300

    Rewrite parser all fp like

--- src/parse2.ts
+++ src/parse2.ts
@@ -1,158 +1,98 @@
-import { Message, ClientOptions, Client, MessageContent } from "eris"
-import path from 'path'
+import { words } from 'lodash/fp'
 
-export type CommandExecFn = (
-  data: {
-    message: Message,
-    args: string[],
-    flags: { [key: string]: string | number | boolean },
-    meta: { [key:string]: any }
-  }, 
-  reply: (content: MessageContent) => void) => void | string
-
-export type CommandPermissionFn = (author: string, guild: string) => boolean
-
-type a = (val: Number) => void
-
-// Flag
-export type Flag = {
+export type parseData = {
+  prefix: string
   name: string,
-  long: string,
-  short?: string,
-  type: 'bool' | 'number' | 'string'
+  args: string[],
+  flags: {[key: string]: any}
+} | null
+
+export type Parser = {
+  data: parseData,
+  input: string,
+  chain: (f: (data: parseData, input: string) => Parser) => Parser
 }
 
-// Command
-export interface CommandOpts {
-  checkPermissionFn?: (author: string, channel: string) => boolean
-  meta?: { [key: string]: any}
+export type FlagRules = {
+  [key: string]: boolean
 }
 
-export class Command {
-  flags: { [key: string]: Flag } = {}
-  meta: { [key: string]: any} = {}
-  exec: CommandExecFn
-  checkPermission: CommandPermissionFn = (author:string, guild: string) => true
-  registerFlag: (name: string, short?: string, type?: 'bool' | 'number' | 'string') => Command = RegisterFlag
-
-  constructor(fn: CommandExecFn, opts: CommandOpts) {
-    this.exec = fn
-    if (opts.checkPermissionFn != null) this.checkPermission = opts.checkPermissionFn
-    if (opts.meta) this.meta = opts.meta
+function makeParser(data: parseData, input?: string): Parser {
+  return {
+    data: data,
+    input: input ?? '',
+    chain: (f) => f(data, input ?? '')
   }
 }
 
-function RegisterFlag (this: Command, name: string, short?: string, type?: 'bool' | 'number' | 'string'): Command {
-  const flag = {
-    name: name,
-    long: '--' + name,
-    short: short ? '-' + short : undefined,
-    type: type ? type : 'bool',
-  }
-  this.flags[flag.long] = flag
-  if (flag.short != null) this.flags[flag.short] = flag
-  return this
+const emptyData = {
+  name: '',
+  prefix: '',
+  args: [],
+  flags: {}
 }
 
-// Client
-export type ConstructorOptions = ClientOptions & {
-  prefix: string[],
-  admins?: { [key: string]: Set<string> }
+export function parser(pref: RegExp, flagRules: {}) {
+  return (input: string) => {
+    const { data } =
+      makeParser({...emptyData}, input)
+        .chain(extractPrefix(pref))
+        .chain(extractCommand)
+        .chain(extractFlags(flagRules))
+        .chain(extractArgs)
+    return data
+  }
 }
 
-export class PosixClient extends Client {
-  commands: { [key: string]: Command }
-  prefix: string[]
-  prefixrx: RegExp
-  admins: { [key: string]: Set<string> }
-  init: { [key: string]: () => void } = {}
+function extractPrefix(prx: RegExp): (data: parseData, input: string) => Parser {
+  return (data: parseData, input: string): Parser => {
+    if (data == null || input == null) return makeParser(null)
 
-  constructor (token: string, opts?: ConstructorOptions) {
-    super(token, opts)
-    this.prefix = opts?.prefix ?? []
-    this.commands = {}
-    this.prefixrx = new RegExp(`^${this.prefix.join('|')}`, 'g')
-    this.admins = opts?.admins ?? {}
-    this.on('messageCreate', this.handleMessages)
+    const rgxm = input.match(prx)
+    if (rgxm == null || rgxm.length < 2) return makeParser(null)
+
+    data.prefix = rgxm[1]
+
+    return makeParser(data, input.slice(data.prefix.length))
   }
+}
 
-  registerCommand (identifier: string | string[], exec: CommandExecFn, opts: CommandOpts = {}): Command {
-    if (!Array.isArray(identifier)) identifier = [identifier]
-    const com = new Command(exec, opts)
-    identifier.forEach(id => {
-      this.commands[id] = com
-    });
-    return com
-  }
+function extractCommand(data: parseData, input: string): Parser {
+  if (data == null) return makeParser(null)
 
-  registerInit (identifier: string, fn: () => void): void {
-    this.init[identifier] = fn
-  }
+  const w = words(input)
+  const c = w.shift()
 
-  deregisterInit (identifier: string): void {
-    if (this.init[identifier] != null)
-      delete this.init[identifier]
-    else
-      console.error('Tried to remove unexsistent init: ' + identifier)
-  }
+  if (c == null) return makeParser(null)
 
-  handleMessages (message: Message): void {
-    const { author, channel } = message
-    // Set author, channel context
-    // Do context stuff
-    
-    // Check prefix and remove it
-    const prefix = message.content.match(this.prefixrx)?.shift()
-    const content = message.content.slice(prefix?.length)
+  data.name = c
 
-    // Separate words
-    const tokens = content.match(/".*"|[^\s]+/g) ?? []
-    
-    // Handle message
-    if (prefix != null && prefix.length > 0) {
-      const commandName = tokens.shift()
-      const command = commandName ? this.commands[commandName] : undefined
-      if (commandName == null || commandName.length === 0 || command == null) return
-      if (!command.checkPermission(author.id, channel.id)) return
-  
-      const args = []
-      const flags: { [key: string]: string | number | boolean } = {}
-  
-      tokenLoop:
-      for (let i = 0; i < tokens.length; i++) {
-        const token = tokens[i]
-        const flagMap = {
-          string: () => tokens[++i],
-          number: () => Number.parseFloat(tokens[++i]),
-          bool: () => true
+  return makeParser(data, w.join(' '))
+}
+
+function extractFlags(flagRules: FlagRules): (data: parseData, input: string) => Parser {
+  return (data: parseData, input: string) => {
+    if (data == null) return makeParser(null)
+
+    const ws = words(input)
+    const out: string[] = []
+
+    for (let i = 0; i < ws.length; i++) {
+      const w = ws[i];
+      if (w.match(/^--/)) {
+        if (flagRules[w] != null) {
+          data.flags[w] = flagRules[w] ? ws[++i] : true
+          continue
         }
-  
-        if (token.startsWith('--')) {
-          // Check flag exists and add it to list
-          const flag = command?.flags[token] ?? undefined
-          if (flag != null) {
-            flags[flag.name] = flagMap[flag.type]()
-            continue tokenLoop
-          }
-        } else if (token.startsWith('-')) {
-          // Iterate through characters, ignore first (-)
-          // Check flag exists and add it to list
-          const chars = token.split('')
-          for (let j = 1; j < chars.length; j++) {
-            const flag = command?.flags['-' + chars[j]] ?? undefined
-            if (flag != null) {
-              if (flag.type !== 'bool' && chars.length > 2) throw new Error ('Tried to use flag with argument in combined shortcut form')
-              flags[flag.name] = flagMap[flag.type]()
-              continue tokenLoop
-            }
-          }
-        }
-        args.push(token)
       }
-  
-      // Do the thing
-      const reply = command.exec({ message, args, flags, meta: command.meta }, channel.createMessage.bind(channel))
-      if (reply) channel.createMessage(reply)
+      out.push(w)
     }
+    return makeParser(data, out.join(' '))
   }
+}
+
+function extractArgs(data: parseData, input: string): Parser {
+  if (data == null) return makeParser(null)
+  data.args = words(input)
+  return makeParser(data)
 }
