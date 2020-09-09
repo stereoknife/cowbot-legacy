0ca12ad78487f45f7345a3e4cad97ab3a82e3a5b
kam <git@techniworm.dev>
2020-09-09 16:24:57 +0300

    Log function takes any type now

--- src/logging.ts
+++ src/logging.ts
@@ -2,7 +2,7 @@ const logging = true
 
 const level = 1;
 
-export default function log(message: string, priority: number = 1) {
+export default function log(message: any, priority: number = 1) {
   if (logging && priority >= level) {
     console.log(message)
     return
